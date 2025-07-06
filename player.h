7e09f7a71e8dedf9beb4eb14fe09e5fb66da123d
hdddzvl <00126525@uca.edu.sv>
2025-07-06 16:05:15 -0600

    player .h

---
+++ player.h
@@ -0,0 +1,29 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <vector>
+#include <string>
+#include "deck.h" 
+
+using namespace std;
+
+// Class Player para gestionar la mano y lógica del jugador
+class Player {
+public:
+    string name;
+    vector<Card> hand; // Almacena las cartas en la mano del jugador
+
+    // Constructor: Inicializa un jugador con un nombre dado
+    Player(string n);
+
+    // Recibe una carta y la añade a la mano del jugador
+    void receiveCard(Card c);
+
+    // Calcula el total de puntos de la mano del jugador
+    // Marcado como const porque no modifica el estado del objeto Player
+    int calculatePoints() const;
+
+    // Muestra las cartas en la mano del jugador
+    void displayHand() const; 
+};
+
+#endif
