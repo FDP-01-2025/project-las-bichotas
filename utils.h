14b6081297158b95bf873152aeadfa5ed222def5
hdddzvl <00126525@uca.edu.sv>
2025-07-06 18:59:34 -0600

    utils.h

--- utils.h
+++ utils.h
@@ -14,3 +14,20 @@ using namespace std;
 #define MAGENTA_COLOR "\033[35m"
 #define CYAN_COLOR    "\033[36m"
 
+// Variables globales de color para resultados del juego
+extern string WINNER_COLOR;
+extern string LOSER_COLOR;
+
+// Muestra un mensaje en la consola con un color opcional
+void displayMessage(const string& message, const string& color = RESET_COLOR);
+
+// Reproduce un sonido de beep del sistema
+void beepSound();
+
+// Muestra la información de una carta en formato de texto (ejemplo: "[As de Espadas]")
+void displayCardText(const string& rank, const string& suit);
+
+// Muestra un mensaje de logro con un color especial y sonido
+void displayAchievement(const string& achievement);
+
+#endif // UTILS_H
