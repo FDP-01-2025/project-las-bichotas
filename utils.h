002f7a53f76836e6b107ccf218525ef9a51ab92f
hdddzvl <00126525@uca.edu.sv>
2025-07-07 13:24:41 -0600

    utils.h

--- utils.h
+++ utils.h
@@ -13,3 +13,14 @@ using namespace std;
 #define BLUE_COLOR    "\033[34m"
 #define MAGENTA_COLOR "\033[35m"
 #define CYAN_COLOR    "\033[36m"
+
+// Variables globales de color para resultados del juego
+extern string WINNER_COLOR;
+extern string LOSER_COLOR;
+
+// Muestra un mensaje en la consola con un color opcional
+void displayMessage(const string& message, const string& color = RESET_COLOR);
+
+
+
+#endif // UTILS_H
