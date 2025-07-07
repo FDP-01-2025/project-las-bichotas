aac68a76e7c40e52e233225c26bc4409edf62aac
hdddzvl <00126525@uca.edu.sv>
2025-07-07 12:43:56 -0600

    utils.h

---
+++ utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+using namespace std;
+
+// Códigos de escape ANSI para colores en la consola
+#define RESET_COLOR   "\033[0m"
+#define RED_COLOR     "\033[31m"
+#define GREEN_COLOR   "\033[32m"
+#define YELLOW_COLOR  "\033[33m"
+#define BLUE_COLOR    "\033[34m"
+#define MAGENTA_COLOR "\033[35m"
+#define CYAN_COLOR    "\033[36m"
